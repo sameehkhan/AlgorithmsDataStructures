d5bddb721f640b426116defe414ea328e17c5477
Sameeh <sameehkhan1@gmail.com>
2019-02-25 16:08:50 -0800

    Depth First Search

--- prep.js
+++ prep.js
@@ -131,3 +131,31 @@ Array.prototype.myJoin = function (separator = '') {
     });
     return str;
 }
+
+// Do not edit the class below except
+// for the depthFirstSearch method.
+// Feel free to add new properties
+// and methods to the class.
+class Node {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
+
+    addChild(name) {
+        this.children.push(new Node(name));
+        return this;
+    }
+
+    depthFirstSearch(array) {
+        // Write your code here.
+        array.push(this.name)
+        for (let i = 0; i < this.children.length; i++) {
+            this.children[i].depthFirstSearch(array)
+        }
+        return array;
+    }
+}
+
+// Do not edit the line below.
+exports.Node = Node;
